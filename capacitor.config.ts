ac5273bfe220f0950408b91a209400bcacf155ce
Bismay.exe <bismaysundarmahanta@gmail.com>
2026-02-14 03:30:59 +0530

    xyz

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nobadfonts.app',
+  appName: 'NoBadFonts',
+  webDir: 'dist'
+};
+
+export default config;
